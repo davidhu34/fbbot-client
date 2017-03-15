e028deafacb3e95bbd8d93308b8e24b914870a3d
davidhu34 <b01901133@gmail.com>
2017-03-15 20:43:04 +0800

    webhook hit

--- server/index.js
+++ server/index.js
@@ -4,18 +4,41 @@ const http = require('http')
 const path = require('path')
 const express = require('express')
 const fbbot = require('messenger-bot')
-
-
+const botly = require('botly')
 
 const { normalizePort, onError, onListening } = require('./util')
 const applyMiddlewares = require('./middlewares')
 
-let bot = new fbbot({
+const app = express()
+applyMiddlewares( app )
+const PORT = 3005
+const port = normalizePort( process.env.PORT || PORT )
+app.set( 'port', port )
+app.use( express.static( path.join( __dirname, '..', 'public' ) ) )
+
+const bot = new botly({
+    verifyToken: "verification",
+    accessToken: "EAAJ1qMtnld8BAFfoNmK8cNWdZC1smVKB8DknlENZADZAUlhG0ZButwJC9oqVPj8Jz6DOJbvBLxOQBZANBHz1TML3lomI6bsIegA7LUvLeQlRmT7OxOi9t7yitEw8IFQBnudDBp8sLFnOK9ydZBzOprxQCEu0Jejcg84excAh1BNQZDZD"
+})
+bot.on('message', (sender, message, data) => {
+	console.log('receive log', sender, message, data)
+	bot.send({
+		id: sender,
+		message:{
+			text: 'loolol'	
+		}
+	}, (err, data) => {
+		if (err) console.log('err:', err)
+		else console.log('msg sent:', data)
+	})
+})
+
+app.use('/fbwebhook', bot.router())
+/*let bot = new fbbot({
 	token: 'EAAJ1qMtnld8BAFfoNmK8cNWdZC1smVKB8DknlENZADZAUlhG0ZButwJC9oqVPj8Jz6DOJbvBLxOQBZANBHz1TML3lomI6bsIegA7LUvLeQlRmT7OxOi9t7yitEw8IFQBnudDBp8sLFnOK9ydZBzOprxQCEu0Jejcg84excAh1BNQZDZD',
 	verify: 'verification',
 	app_secret: 'secret'
 })
-
 bot.on('message', (payload, reply, actions) => {
 	console.log('got msg', payload)
 	reply({text:'dude'}, () => {console.log('replied')} )
@@ -23,20 +46,13 @@ bot.on('message', (payload, reply, actions) => {
 bot.on('error', (err) => {
   console.log(err.message)
 })
-//http.createServer(bot.middleware()).listen(3000)
-
-const app = express()
-applyMiddlewares( app )
-const PORT = 3006
-const port = normalizePort( process.env.PORT || PORT )
-app.set( 'port', port )
-app.use( express.static( path.join( __dirname, '..', 'public' ) ) )
+http.createServer(bot.middleware()).listen(3000)
 app.get('/webhook', (req, res) => {
   return bot._verify(req, res)
-})
+})*/
+
 
 // web port
-
 app.get( '/', (req, res, next) => {
     res.sendFile( path.join( __dirname, '..', 'index.html' ) )
 })
@@ -44,5 +60,6 @@ app.get( '/', (req, res, next) => {
 
 const server = http.createServer( app )
 server.listen( port )
+console.log('on')
 server.on( 'error', onError( port ) )
 server.on( 'listening', onListening( server ) )
